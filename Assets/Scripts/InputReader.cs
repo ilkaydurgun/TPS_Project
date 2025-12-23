330ca261b862cb4057c982c85e5523c0abfb40d1
ilkaydurgun <ilkydrgun99@gmail.com>
2025-12-23 12:27:05 +0300

    Refactor player input handling and state management
    - Removed obsolete Control.cs file and its meta.
    - Added InputReader class to manage player input events for Jump and Dodge actions.
    - Updated PlayerStateMachine to include InputReader as a serialized field.
    - Modified PlayerTestState to handle Jump events and manage state transitions.
    - Updated project settings with new cloud project ID and organization ID.
    - Regenerated Control.cs from input actions asset, ensuring proper input handling.
    - Updated EditorBuildSettings with new input action GUID.

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+
+{
+    public event Action DodgeEvent;
+    public event Action JumpEvent;
+    private Controls controls;
+
+    void Start()
+    {
+        controls = new Controls();
+        controls.Player.SetCallbacks(this);
+        controls.Player.Enable();
+    }
+    private void OnDestroy()
+    {
+        controls.Player.Disable();  
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed){ return; }
+        JumpEvent?.Invoke();
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+         if (!context.performed){ return; }
+        DodgeEvent?.Invoke();
+    }
+}
--- Assets/Scripts/StateMachines/PlayerStateMachines/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/PlayerStateMachines/PlayerStateMachine.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
-
+   [field: SerializeField] public InputReader InputReader { get; private set; }
     void Start()
     {
         SwitchState(new PlayerTestState(this));
